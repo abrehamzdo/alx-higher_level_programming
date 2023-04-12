77f31e738e384a1c1768dc7f28746ca13a3bb4ee
abrehamzdo <abrehamzewdu231994@gmail.com>
2023-04-12 08:35:17 -0700

    0-read_file.py

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''
+   function to handle open and read files
+'''
+def read_file(filename=""):
+    '''
+    function to read a file
+    '''
+    with open(filename, encoding='utf-8') as f:
+        print(f.read(), end="")
